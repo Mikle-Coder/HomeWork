95c264eacf967733934268aeb656ce60e8801729
МuKa _ <miha2.015@mail.ru>
2022-11-23 17:03:20 +0300

    Add ITVDN_4_3 and tasks 0 and 2

---
+++ ITVDN_4_3/ITVDN_4_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Создайте на диске 100 директорий с именами от Folder_0 до Folder_99, затем удалите их
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        List<DirectoryInfo> directories = new List<DirectoryInfo>();
+        for (int i = 0; i < 100; i++)
+            directories.Add(Directory.CreateDirectory(@".\Folder_" + i));
+
+        Console.WriteLine("Директории созданы");
+        Console.ReadKey();
+        
+        foreach(DirectoryInfo d in directories)
+            d.Delete();
+
+        Console.WriteLine("Директории удалены");
+    }
+}
---
+++ ITVDN_4_3/Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Создайте файл, запишите в него произвольные данные и закройте файл. Затем снова откройте
+//  этот файл, прочитайте из него данные и выведете их на консоль.
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        FileInfo file = new FileInfo("NewFile.txt");
+        StreamWriter streamWriter = file.CreateText();
+        
+
+        streamWriter.WriteLine("I add new Line1");
+        streamWriter.Write(streamWriter.NewLine);
+        streamWriter.WriteLine("I add new Line3");
+
+        streamWriter.Close();
+
+        StreamReader streamReader = new StreamReader(file.FullName);
+
+        while(true)
+        {
+            string line = streamReader.ReadLine();
+            if (line != null)
+                Console.WriteLine(line);
+            else
+                break;
+        }
+
+        streamReader.Close();
+    }
+}
