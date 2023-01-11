427742a3d440b3ddbdbd9b8ec76c07778768f7bf
Mikle <miha2.015@mail.ru>
2023-01-11 06:42:54 +0300

    Add Assistant

---
+++ Assistant/Assistant/Class1.cs
@@ -0,0 +1,55 @@
+﻿//using Microsoft.Speech.Recognition;
+//using Microsoft.Speech.Recognition.SrgsGrammar;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Jarvis
+//{
+//    internal class Class1
+//    {
+//        static void Main()
+//        {
+//            //Создаем распознаватель с русской локализацией
+//            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("ru-RU"));
+
+//            string grammarPath = @"";
+//            //Компилируем наше грамматическое правило в файл Маршруты.cfg
+//            FileStream fs = new FileStream(grammarPath + "Маршруты.cfg", FileMode.Create);
+//            SrgsGrammarCompiler.Compile(grammarPath + "Маршруты.grxml", (Stream)fs);
+//            fs.Close();
+
+//            Grammar gr = new Grammar(grammarPath + " Маршруты.cfg", "Маршруты");
+
+//            //Загружаем скомпилированный файл грамматики
+//            sre.LoadGrammar(gr);
+
+//            //Подписываемся на событие распознавания
+//            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+
+//            //Пусть голос поступает с устройства по умолчанию
+//            sre.SetInputToDefaultAudioDevice();
+
+//            //Запускаем асинхронно распознаватель
+//            sre.RecognizeAsync(RecognizeMode.Multiple);
+
+//            //Процедура распознавания
+//            void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+//            {
+//                //Распознанная фраза
+//                string recoString = e.Result.Text;
+//                //Имя команды
+//                string cmdName = e.Result.Semantics["cmd"].Value.ToString();
+//                //Точка А маршрута
+//                string pointA = e.Result.Semantics["from"].Value.ToString();
+//                //Точка Б маршрута
+//                string pointB = e.Result.Semantics["to"].Value.ToString();
+//                //Показываем сообщение
+//                MessageBox.Show($"Маршрут от точки А: {pointA} до точки Б: {pointB}");
+//            }
+//        }
+//    }
+//}
---
+++ Assistant/Assistant/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Speech.Recognition;
+using System.Globalization;
+using System.IO;
+using Microsoft.Speech.Recognition.SrgsGrammar;
+using System.Linq;
+
+namespace Jarvis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(CultureInfo.CurrentCulture))
+            {
+                recognizer.SpeechRecognized +=
+                  new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+                // Configure input to the speech recognizer.  
+                recognizer.SetInputToDefaultAudioDevice();
+
+                string directoryPath = "D:\\Хранилище\\source\\Repos\\Jarvis\\Jarvis\\Grammars\\";
+
+                var grammarPaths = Directory.GetFiles(directoryPath).Where(s=>s.EndsWith(".xml"));
+                foreach (string grammarPath in grammarPaths)
+                {
+                    string filename = grammarPath.Substring(grammarPath.LastIndexOf('\\') + 1, grammarPath.IndexOf('.') - grammarPath.LastIndexOf('\\') -1);
+                    FileStream stream = new FileStream(directoryPath + filename + ".cfg", FileMode.Create);
+                    SrgsGrammarCompiler.Compile(grammarPath, stream);
+                    stream.Close();
+
+                    Grammar grammar = new Grammar(directoryPath + filename + ".cfg", filename);
+                    recognizer.LoadGrammar(grammar);
+                }
+
+
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+                // Keep the console window open.  
+                while (true)
+                {
+
+                }
+            }
+        }
+
+        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            var words = e.Result.Words;
+            foreach (var word in words)
+                if(word.Confidence <= 0.3)
+                {
+                    Console.WriteLine("Простите, я вас не поняла");
+                    return;
+                }
+
+            Console.WriteLine(e.Result.Text + "\t:" + e.Result.Confidence);
+
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            foreach(var word in words )
+                Console.WriteLine(word.LexicalForm + "\t:" + word.Confidence);
+            Console.ResetColor();
+            Console.WriteLine("\n\n");
+        }        
+    }
+}
